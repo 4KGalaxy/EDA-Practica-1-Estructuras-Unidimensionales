28137ecde1bee525ee74859988238c172b50035e
ZaimonLPZ <theonewhoisrich@gmail.com>
2026-08-21 22:57:15 -0500

    abc
    efg

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,7 @@
-
+try {
+    BuferPaquetes bufer(C);
+} catch (const std::invalid_argument& e) {
+    std::cerr << "Error: " << e.what() << std::endl;
+    return 1;
+}
+// esto es para despues, pero lo dejo aqui mientras.
